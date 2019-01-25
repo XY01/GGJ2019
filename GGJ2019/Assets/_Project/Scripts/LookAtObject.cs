494611bc17af39ffc132fd204156cd31b79c8439
John Dunning <john.charles.dunning@gmail.com>
2019-01-26 09:25:57 +1100

    Added speech

---
+++ GGJ2019/Assets/_Project/Scripts/LookAtObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtObject : MonoBehaviour
+{
+    public bool _TargetMainCamera;
+    public GameObject _Target;
+    void Update()
+    {
+        if(_TargetMainCamera == true)
+        {
+            transform.LookAt(Camera.main.transform.position);
+        }
+        else
+        {
+            transform.LookAt(_Target.transform.position);
+        }
+       
+    }
+}
---
+++ GGJ2019/Assets/_Project/Scripts/Speech.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speech : MonoBehaviour
+{
+    public string[] _SpeechText;
+    public Text _OutputSpeech;
+    public Animator _SpeechAnim;
+    public GameObject _Echidna;
+    float timer = 0;
+    int randoTimeTillTalk;
+    private void Start()
+    {
+        SetRandoTime();
+    }
+    private void Update()
+    {
+        transform.position = _Echidna.transform.position;
+        timer += Time.deltaTime;
+        if (timer >= randoTimeTillTalk)
+        {
+            Talk();
+            timer = 0;
+        }
+    }
+    void SetRandoTime()
+    {
+        randoTimeTillTalk = Random.Range(4, 20);
+    }
+    void Talk()
+    {
+        int selectedSpeech = Random.Range(0, _SpeechText.Length);
+        _OutputSpeech.text = _SpeechText[selectedSpeech];
+        _SpeechAnim.SetTrigger("Talk");
+        SetRandoTime();
+    }
+}
+
