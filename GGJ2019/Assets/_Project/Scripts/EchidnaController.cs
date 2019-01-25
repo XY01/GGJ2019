3c0ce72cdfaa5d8df7ba898653a7b46007d05595
Brad Hammond <brad@xy01.net>
2019-01-25 21:47:15 +1100

    few scripts and folders

---
+++ GGJ2019/Assets/_Project/Scripts/EchidnaController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EchidnaController : MonoBehaviour
+{
+    public float _ControllableRadius = 1.5f;
+
+    float _CurrentScale = .4f;
+    float _BaseScale = .4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position, transform.localScale.x * _ControllableRadius);
+    }
+}
---
+++ GGJ2019/Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum ControlType
+    {
+        Physics,
+        Kinematic,    
+    }
+
+    public ControlType _ControlType = ControlType.Kinematic;
+
+    Rigidbody _RB;
+
+    Vector3 _Pos;
+    public float _Speed = 10;
+    Vector3 _InputVector;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _RB = GetComponent<Rigidbody>();
+        _RB.isKinematic = true;
+
+        _Pos = transform.position;
+    }
+
+    // Update is called once per fram
+    void Update()
+    {
+        _InputVector.x = Input.GetAxis("Horizontal");
+        _InputVector.z = Input.GetAxis("Vertical");
+
+        _Pos += _InputVector * Time.deltaTime;
+
+
+        transform.position = _Pos;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + _InputVector);
+    }
+}
