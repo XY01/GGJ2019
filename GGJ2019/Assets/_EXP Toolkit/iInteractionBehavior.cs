5db610d95024fd13c487830f66122a14d603ebd5
Brad Hammond <brad@xy01.net>
2019-01-25 17:16:30 +1100

    fixed bug

---
+++ GGJ2019/Assets/_EXP Toolkit/iInteractionBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+namespace EXPToolkit
+{
+    public interface iInteractionBehavior
+    {
+        void BeginHover();
+        event EventHandler OnBeginHover;
+        void StayHovered();
+        event EventHandler OnStayHovered;
+        void EndHover();
+        event EventHandler OnEndHover;
+
+        void BeginGrasp();
+        event EventHandler OnBegingGrasp;
+        void StayGrasped();
+        event EventHandler OnStayGrasped;
+        void EndGrasped();
+        event EventHandler OnEndGrasp;
+    }
+}
