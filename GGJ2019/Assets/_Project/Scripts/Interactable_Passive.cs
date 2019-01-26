4bac0080368a2beb7282e9d624fa852e8db72f6e
Steve Hadley <steven.dale.hadley@gmail.com>
2019-01-26 14:22:40 +1100

    Adding passive interactable class

---
+++ GGJ2019/Assets/_Project/Scripts/Interactable_Passive.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_Passive : MonoBehaviour, iInteractable
+{
+    PlayerController _InteractingPlayer;
+
+    #region Interactable interface methods
+    public GameObject GetGameObject()
+    {
+        return gameObject;
+    }
+
+    public void BeginInteraction(PlayerController player)
+    {
+        _InteractingPlayer = player;
+
+        print("Interaction begun");
+    }
+
+    public void ContinueInteraction(PlayerController player)
+    {
+    }
+
+    public void EndInteraction(PlayerController player)
+    {
+
+    }
+    #endregion
+}
