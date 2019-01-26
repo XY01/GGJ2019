c3ca6813191aa6d38160b524da39c8db18c9f945
Steve Hadley <steven.dale.hadley@gmail.com>
2019-01-27 08:58:36 +1100

    Sending score screen to John

--- GGJ2019/Assets/_Project/Scripts/Menu.cs
+++ GGJ2019/Assets/_Project/Scripts/Menu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UIMethods : MonoBehaviour
+public class Menu : MonoBehaviour
 {
     public void StartGame()
     {
