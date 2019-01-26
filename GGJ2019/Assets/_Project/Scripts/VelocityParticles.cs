42d20b66b6303ac52e15b0875289485987443f08
John Dunning <john.charles.dunning@gmail.com>
2019-01-26 17:45:03 +1100

    Echidna dust particles

---
+++ GGJ2019/Assets/_Project/Scripts/VelocityParticles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityParticles : MonoBehaviour
+{
+    public Rigidbody _Rb;
+    private ParticleSystem _Ps;
+    private ParticleSystem.EmissionModule _PsEmmission;
+
+    private void Start()
+    {
+        _Ps = GetComponent<ParticleSystem>();
+        _PsEmmission = _Ps.emission;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        _PsEmmission.rate = _Rb.velocity.magnitude * 30;
+        Debug.Log(_Rb.velocity.magnitude);
+    }
+}
